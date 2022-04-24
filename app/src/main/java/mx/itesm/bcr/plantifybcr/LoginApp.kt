4ff6a88a9cee6ab71320ca2cf1576b8d16b1a11c
Bruno <omarjm0910@gmail.com>
2022-04-24 13:54:41 -0500

    se creo la pantalla de login aunque aun no tiene funcionalidad y le falta diseño y se conecto la app con firebase

---
+++ app/src/main/java/mx/itesm/bcr/plantifybcr/LoginApp.kt
@@ -0,0 +1,11 @@
+package mx.itesm.bcr.plantifybcr
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class LoginApp : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login_app)
+    }
+}
