ad99874130f434f995b6e4a7db8d7f15d4d0ddbf
Bruno <omarjm0910@gmail.com>
2022-04-25 10:22:13 -0500

    Ya se puede hacer login a la app, falta implementar el logout por un error(preguntar al profe)

--- app/src/main/java/mx/itesm/bcr/plantifybcr/AgregarPlantaFrag.kt
+++ app/src/main/java/mx/itesm/bcr/plantifybcr/AgregarPlantaFrag.kt
@@ -1,15 +1,18 @@
 package mx.itesm.bcr.plantifybcr
 
+import android.content.Intent
 import androidx.lifecycle.ViewModelProvider
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.firebase.ui.auth.AuthUI
+import mx.itesm.bcr.plantifybcr.databinding.FragmentHomeBinding
+import mx.itesm.bcr.plantifybcr.ui.home.HomeViewModel
 import mx.itesm.bcr.plantifybcr.viewmodels.AgregarPlantaVM
 
 class AgregarPlantaFrag : Fragment() {
-
     companion object {
         fun newInstance() = AgregarPlantaFrag()
     }
@@ -29,4 +32,26 @@ class AgregarPlantaFrag : Fragment() {
         // TODO: Use the ViewModel
     }
 
+
 }
+/*private var _binding: FragmentHomeBinding? = null
+    private val binding get() = _binding!!
+
+
+    private lateinit var viewModel: AgregarPlantaVM
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val homeViewModel = ViewModelProvider(this).get(HomeViewModel::class.java)
+        _binding = FragmentHomeBinding.inflate(inflater,container,false)
+        val root: View = binding.root
+        AuthUI.getInstance().signOut(requireContext()).addOnCompleteListener {
+            activity?.finish()
+            val intLogin = Intent(requireContext(), LoginApp::class.java)
+            startActivity(intLogin)
+        }
+    return root
+    }
+*/
--- app/src/main/java/mx/itesm/bcr/plantifybcr/LoginApp.kt
+++ app/src/main/java/mx/itesm/bcr/plantifybcr/LoginApp.kt
@@ -1,11 +1,71 @@
 package mx.itesm.bcr.plantifybcr
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.firebase.ui.auth.AuthUI
+import com.firebase.ui.auth.FirebaseAuthUIActivityResultContract
+import com.firebase.ui.auth.data.model.FirebaseAuthUIAuthenticationResult
+import com.google.firebase.auth.FirebaseAuth
+import mx.itesm.bcr.plantifybcr.databinding.ActivityLoginAppBinding
 
 class LoginApp : AppCompatActivity() {
+    private lateinit var binding: ActivityLoginAppBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login_app)
+        binding = ActivityLoginAppBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.btnLogin.setOnClickListener{
+            println("Comenzando autenticacion")
+            autenticar()
+        }
+        verificarLogin()
+    }
+
+    private fun verificarLogin() {
+        val usuario = FirebaseAuth.getInstance().currentUser
+        if(usuario != null){
+            println("Bienvenido")
+            entrarApp()
+        }
+    }
+
+    private val signInLauncher = registerForActivityResult(
+
+        FirebaseAuthUIActivityResultContract()
+    ) {
+            res ->
+        this.onSignInResult(res)
+    }
+    private fun onSignInResult(result: FirebaseAuthUIAuthenticationResult) {
+        val response = result.idpResponse
+        if(result.resultCode == RESULT_OK){
+            val usuario = FirebaseAuth.getInstance().currentUser
+            println("Bienvenido: ${usuario?.displayName}")
+            println("Correo: ${usuario?.email}")
+            println("Token: ${usuario?.uid}")
+            entrarApp()
+        }else{
+            println("Error en tus datos")
+        }
+    }
+
+    private fun entrarApp() {
+        val intApp = Intent(this,MainActivity::class.java)
+        startActivity(intApp)
+        //Borrar pantalla login
+        finish()
+    }
+
+    private fun autenticar() {
+        print("ENtramos a autenticar")
+        val providers = arrayListOf(
+            AuthUI.IdpConfig.GoogleBuilder().build(),
+        )
+        val signInIntent = AuthUI.getInstance()
+            .createSignInIntentBuilder()
+            .setAvailableProviders(providers)
+            .build()
+        signInLauncher.launch(signInIntent)
     }
 }
