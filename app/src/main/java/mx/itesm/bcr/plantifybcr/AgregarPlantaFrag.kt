dcf70c9144b3479af2da9126c3a16d2e6ba049f9
CarlosGarciaG <a01748393@tec.mx>
2022-05-03 13:27:05 -0500

    DashboarArreglado

--- app/src/main/java/mx/itesm/bcr/plantifybcr/AgregarPlantaFrag.kt
+++ app/src/main/java/mx/itesm/bcr/plantifybcr/AgregarPlantaFrag.kt
@@ -1,23 +1,36 @@
 package mx.itesm.bcr.plantifybcr
 
+import android.app.Activity.RESULT_OK
+import android.app.Notification
 import android.content.Intent
+import android.net.Uri
 import androidx.lifecycle.ViewModelProvider
 import android.os.Bundle
+import android.provider.MediaStore
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageButton
+import android.widget.ImageView;
 import com.firebase.ui.auth.AuthUI
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ktx.database
+import com.google.firebase.ktx.Firebase
 import mx.itesm.bcr.plantifybcr.databinding.FragmentHomeBinding
 import mx.itesm.bcr.plantifybcr.ui.home.HomeViewModel
+import mx.itesm.bcr.plantifybcr.ui.notifications.NotificationsFragment
+import android.Manifest.permission.CAMERA
+import android.Manifest.permission.WRITE_EXTERNAL_STORAGE
+import android.content.Context
+import mx.itesm.bcr.plantifybcr.databinding.ActivityLoginAppBinding
+import mx.itesm.bcr.plantifybcr.databinding.AgregarPlantaFragmentBinding
+
 import mx.itesm.bcr.plantifybcr.viewmodels.AgregarPlantaVM
 
-class AgregarPlantaFrag : Fragment() {
-    companion object {
-        fun newInstance() = AgregarPlantaFrag()
-    }
+class AgregarPlantaFrag : Fragment(), OnFragmentActionsListener {
 
-    private lateinit var viewModel: AgregarPlantaVM
+    private var listener: OnFragmentActionsListener? = null
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -26,31 +39,26 @@ class AgregarPlantaFrag : Fragment() {
         return inflater.inflate(R.layout.agregar_planta_fragment, container, false)
     }
 
-    override fun onActivityCreated(savedInstanceState: Bundle?) {
-        super.onActivityCreated(savedInstanceState)
-        viewModel = ViewModelProvider(this).get(AgregarPlantaVM::class.java)
-        // TODO: Use the ViewModel
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        //addImg.setOnClickListener { listener?.onClickFragmentButton() }
     }
 
-}
-/*private var _binding: FragmentHomeBinding? = null
-    private val binding get() = _binding!!
-
-
-    private lateinit var viewModel: AgregarPlantaVM
-
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        val homeViewModel = ViewModelProvider(this).get(HomeViewModel::class.java)
-        _binding = FragmentHomeBinding.inflate(inflater,container,false)
-        val root: View = binding.root
-        AuthUI.getInstance().signOut(requireContext()).addOnCompleteListener {
-            activity?.finish()
-            val intLogin = Intent(requireContext(), LoginApp::class.java)
-            startActivity(intLogin)
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+        if (context is OnFragmentActionsListener) {
+            listener = context
         }
-    return root
     }
-*/
+
+    override fun onDetach() {
+        super.onDetach()
+        listener = null
+    }
+
+    override fun onClickFragmentButton() {
+        TODO("Not yet implemented")
+    }
+}
+
+
--- app/src/main/java/mx/itesm/bcr/plantifybcr/ui/dashboard/DashboardFragment.kt
+++ app/src/main/java/mx/itesm/bcr/plantifybcr/ui/dashboard/DashboardFragment.kt
@@ -5,22 +5,15 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
-import androidx.navigation.fragment.findNavController
-import androidx.recyclerview.widget.LinearLayoutManager
 import com.firebase.ui.auth.AuthUI
-import mx.itesm.bcr.plantifybcr.ListenerRecycler
 import mx.itesm.bcr.plantifybcr.LoginApp
 import mx.itesm.bcr.plantifybcr.databinding.FragmentDashboardBinding
-import mx.itesm.bcr.plantifybcr.ui.home.HomeFragmentDirections
-import mx.itesm.bcr.plantifybcr.viewmodels.plantaWikiAdaptador
 
-class DashboardFragment : Fragment(),ListenerRecycler {
+class DashboardFragment : Fragment() {
 
     private var _binding: FragmentDashboardBinding? = null
-    private lateinit var adaptador: plantaWikiAdaptador
 
     // This property is only valid between onCreateView and
     // onDestroyView.
@@ -36,21 +29,15 @@ class DashboardFragment : Fragment(),ListenerRecycler {
 
         _binding = FragmentDashboardBinding.inflate(inflater, container, false)
         val root: View = binding.root
-        val recyclerView = _binding?.rvPlantaWiki
-        adaptador = plantaWikiAdaptador()
-
-        recyclerView?.layoutManager = LinearLayoutManager(this.requireContext())
-        recyclerView?.adapter = adaptador
-        adaptador.listener = this
-
+        binding.btnLogOut2.setOnClickListener {
+            println("SALIENDO DE TU SESION")
+            AuthUI.getInstance().signOut(requireContext()).addOnCompleteListener {
+                activity?.finish()
+                val intLogin = Intent(requireContext(), LoginApp::class.java)
+                startActivity(intLogin)
+            }
+        }
         return root
-    }
-
-    override fun itemClicked(position: Int) {
-        val plantaWiki = adaptador.titles[position]
-        println("click en $plantaWiki")
-        val accion = DashboardFragmentDirections.actionWikiFragToInfoWikiPlantaFrag(plantaWiki)
-        findNavController().navigate(accion)
     }
 
     override fun onDestroyView() {
