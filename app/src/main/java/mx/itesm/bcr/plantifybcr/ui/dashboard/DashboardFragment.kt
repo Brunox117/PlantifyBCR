d5e9f6f0ab8831e27a2fb5f944a3334de54b32d2
Bruno <omarjm0910@gmail.com>
2022-05-11 15:36:43 -0500

    Se recupero la wiki

--- app/src/main/java/mx/itesm/bcr/plantifybcr/ui/dashboard/DashboardFragment.kt
+++ app/src/main/java/mx/itesm/bcr/plantifybcr/ui/dashboard/DashboardFragment.kt
@@ -1,20 +1,25 @@
 package mx.itesm.bcr.plantifybcr.ui.dashboard
 
-import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.firebase.ui.auth.AuthUI
+import mx.itesm.bcr.plantifybcr.ListenerRecycler
 import mx.itesm.bcr.plantifybcr.LoginApp
 import mx.itesm.bcr.plantifybcr.databinding.FragmentDashboardBinding
+import mx.itesm.bcr.plantifybcr.ui.home.HomeFragmentDirections
+import mx.itesm.bcr.plantifybcr.viewmodels.plantaWikiAdaptador
 
-class DashboardFragment : Fragment() {
+class DashboardFragment : Fragment(),ListenerRecycler {
 
     private var _binding: FragmentDashboardBinding? = null
-
+    private lateinit var adaptador: plantaWikiAdaptador
     // This property is only valid between onCreateView and
     // onDestroyView.
     private val binding get() = _binding!!
@@ -29,15 +34,20 @@ class DashboardFragment : Fragment() {
 
         _binding = FragmentDashboardBinding.inflate(inflater, container, false)
         val root: View = binding.root
-        binding.btnLogOut2.setOnClickListener {
-            println("SALIENDO DE TU SESION")
-            AuthUI.getInstance().signOut(requireContext()).addOnCompleteListener {
-                activity?.finish()
-                val intLogin = Intent(requireContext(), LoginApp::class.java)
-                startActivity(intLogin)
-            }
-        }
+        val recyclerView = _binding?.rvPlantaWiki
+        adaptador = plantaWikiAdaptador()
+
+        recyclerView?.layoutManager = LinearLayoutManager(this.requireContext())
+        recyclerView?.adapter = adaptador
+        adaptador.listener = this
         return root
+    }
+
+    override fun itemClicked(position: Int) {
+        val plantaWiki = adaptador.titles[position]
+        println("click en $plantaWiki")
+        val accion = DashboardFragmentDirections.actionWikiFragToInfoWikiPlantaFrag(plantaWiki)
+        findNavController().navigate(accion)
     }
 
     override fun onDestroyView() {
