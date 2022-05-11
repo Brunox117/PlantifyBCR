e8ff6cacb013eeb8f984ae749870c23e9cad68c9
Bruno <omarjm0910@gmail.com>
2022-05-11 18:12:38 -0500

    Cambios en los arrays de plantas

--- app/src/main/java/mx/itesm/bcr/plantifybcr/Planta.kt
+++ app/src/main/java/mx/itesm/bcr/plantifybcr/Planta.kt
@@ -6,7 +6,8 @@ import java.time.LocalTime
 data class Planta(
     var nombre: String="",
     var hora: String="",
-    var iluminacion: String=""
+    var iluminacion: String="",
+    var grupo: String="Esta planta no pertenece a ningun grupo"
     /*var foto: Int=0,
     var temperaturaIdeal: Int=0,
     var humedadIdeal: Int=0,
